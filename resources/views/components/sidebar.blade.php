c71ac7c009977de8006b661908d70c4e6ad5d16a
Victorninn <victorninn@gmail.com>
2026-04-04 00:47:20 +0800

    adding favicon and logo

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -10,11 +10,15 @@
     <div class="flex flex-col h-full">
         <!-- Logo -->
         <div class="p-6 border-b border-slate-100">
-            <a href="{{ route('dashboard') }}" class="flex items-center gap-3">
-                <div class="w-10 h-10 bg-gradient-to-br from-teal-400 to-sky-500 rounded-xl flex items-center justify-center">
-                    <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <a href="{{ route('dashboard') }}" class="flex items-center gap-2">
+                  <!--<div class="w-10 h-10 bg-gradient-to-br from-teal-400 to-sky-500 rounded-xl flex items-center justify-center">
+                  <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"/>
                     </svg>
+                </div>-->
+
+                <div class="w-10 h-10 rounded-xl flex items-center justify-center">
+                    <img src="{{ asset('favicon.svg') }}">
                 </div>
                 <div>
                     <span class="font-bold text-slate-800">goLaundry</span>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard') - {{ config('app.name', 'Laundry System') }} </title>
-    
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
     <!-- Tailwind CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
